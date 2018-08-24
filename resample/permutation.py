603e757977f25ad8d90dd25caa24ce043a1bcb9e
Daniel Saxton <daniel.saxton@gmail.com>
2018-08-23 22:00:04 -0500

    Add permutation ANOVA test
    * Also modify t-test docstring

--- resample/permutation.py
+++ resample/permutation.py
@@ -3,13 +3,7 @@ import numpy as np
 
 def ttest(a1, a2, b=1000, dropna=True):
     """
-    Perform permutation two sample t-test.  The
-    t-statistic is calculated for the two samples
-    which are then pooled.  b permutations of the pooled
-    data are taken and the statistic is recalculated
-    for each permutation of the data.  The statistic as
-    well as the proportion of the permutation distribution
-    less than or equal to that statistic are returned.
+    Perform permutation two sample t-test.
 
     Parameters
     ----------
@@ -25,7 +19,7 @@ def ttest(a1, a2, b=1000, dropna=True):
     Returns
     -------
     {'stat', 'prop'} : {float, float}
-        t statistic as well as proportion of permutation 
+        t statistic as well as proportion of permutation
         distribution less than or equal to that statistic
     """
     def g(x, y):
@@ -41,12 +35,60 @@ def ttest(a1, a2, b=1000, dropna=True):
     n1 = len(a1)
     n2 = len(a2)
 
-    X = np.apply_along_axis(func1d=np.random.permutation, 
-                            arr=np.reshape(np.tile(np.append(a1, a2), b), 
-                                           newshape=(b, n1 + n2)), 
+    X = np.apply_along_axis(func1d=np.random.permutation,
+                            arr=np.reshape(np.tile(np.append(a1, a2), b),
+                                           newshape=(b, n1 + n2)),
                             axis=1)
     permute_t = np.apply_along_axis(func1d=lambda s: g(s[:n1], s[n1:]),
                                     arr=X,
                                     axis=1)
 
     return {"stat": t, "prop": np.mean(permute_t <= t)}
+
+
+def anova(*args, b=1000, dropna=True):
+    """
+    Permutation one way analysis of variance.
+
+    Parameters
+    ----------
+    args : sequence of array-like
+        Samples
+    b : int
+        Number of permutations
+    dropna : boolean
+        Whether or not to drop np.nan
+
+    Returns
+    -------
+    {'stat', 'prop'} : {float, float}
+        F statistic as well as proportion of permutation
+        distribution less than or equal to that statistic
+    """
+    args = [np.asarray(a) for a in args]
+
+    if dropna:
+        args = [a[~np.isnan(a)] for a in args]
+
+    t = len(args)
+    ns = [len(a) for a in args]
+    n = np.sum(ns)
+    pos = np.append(0, np.cumsum(ns))
+    arr = np.concatenate(args)
+    gmean = np.mean(arr)
+
+    def g(a):
+        sse = np.sum([ns[i] * np.var(a[pos[i]:pos[i+1]]) for i in range(t)])
+        ssb = (np.sum([ns[i] * (np.mean(a[pos[i]:pos[i+1]]) - gmean)**2
+                       for i in range(t)]))
+        return (ssb / (t - 1)) / (sse / (n - t))
+
+    X = np.reshape(np.tile(arr, b), newshape=(b, n))
+
+    f = g(arr)
+    permute_f = np.apply_along_axis(func1d=(lambda x:
+                                            g(np.random.permutation(x))),
+                                    arr=X,
+                                    axis=1)
+
+    return {"stat": f, "prop": np.mean(permute_f <= f)}
