233d4daaca3898ea93b1c57654e5ac55d4504c9f
Daniel Saxton <daniel.saxton@gmail.com>
2018-09-05 18:31:02 -0500

    Edit setup.py to include README.md

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(name="resample",
-      version="0.1.2",
+      version="0.1.3",
       description="Tools for randomization-based inference in Python",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url="http://github.com/dsaxton/resample",
       author="Daniel Saxton",
       author_email="daniel.saxton@gmail.com",
