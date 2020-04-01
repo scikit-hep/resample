c073c87855038b196854cb4ac8e0a4151c04d61f
Daniel Saxton <dsaxton@pm.me>
2020-04-01 18:39:48 -0500

    Clean tests

--- resample/tests/test_utils.py
+++ resample/tests/test_utils.py
@@ -2,22 +2,17 @@ import pytest
 import numpy as np
 from resample.utils import ecdf, mise, sup_norm
 
-np.random.seed(2357)
 
-x = np.random.randn(100)
-f = ecdf(x)
-
-
-def test_ecdf_incr():
+def test_ecdf_increasing():
+    x = np.random.randn(100)
+    f = ecdf(x)
     result = [f(s) for s in np.linspace(x.min(), x.max(), 100)]
     assert np.all(np.diff(result) >= 0)
 
 
-def test_ecdf_neg_inf():
+def test_ecdf_at_infinity():
+    f = ecdf(np.arange(10))
     assert f(-np.inf) == 0.0
-
-
-def test_ecdf_pos_inf():
     assert f(np.inf) == 1.0
 
 
@@ -29,19 +24,19 @@ def test_ecdf_simple_cases():
     assert g(3) == 1.0
 
 
-def test_mise_inv_domain():
+def test_mise_invalid_domain():
     with pytest.raises(ValueError):
         mise(abs, abs, (1, 0))
 
 
-def test_mise_eq_func():
+def test_mise_identical_functions():
     assert mise(abs, abs, (-3, 3)) == 0.0
 
 
-def test_sup_norm_inv_domain():
+def test_sup_norm_invalid_domain():
     with pytest.raises(ValueError):
         sup_norm(abs, abs, (1, 0))
 
 
-def test_sup_norm_eq_func():
+def test_sup_norm_identical_functions():
     assert sup_norm(abs, abs, (-3, 3)) == 0.0
