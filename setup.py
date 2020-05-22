6ba5c93b3f7a4fcabc35c76a75f98c17a7398aa7
Daniel Saxton <dsaxton@pm.me>
2020-05-21 19:38:57 -0500

    Fix setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,16 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-setup(name="resample",
-      version="0.21",
-      description="Tools for randomization-based inference in Python",
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url="http://github.com/dsaxton/resample",
-      author="Daniel Saxton",
-      author_email="daniel.saxton@gmail.com",
-      license="BSD-3-Clause",
-      packages=["src/resample"],
-      install_requires=["numpy >= 1.14.0", "scipy >= 1.1.0"],
-      zip_safe=False)
+setup(
+    name="resample",
+    version="0.21",
+    description="Tools for randomization-based inference in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://github.com/dsaxton/resample",
+    author="Daniel Saxton",
+    license="BSD-3-Clause",
+    packages=["src/resample"],
+    install_requires=["numpy>=1.17", "scipy>=1.1"],
+    zip_safe=False,
+)
