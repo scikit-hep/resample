62133e320cac81de96e22cd0681b16d825f65ca3
Hans Dembinski <hans.dembinski@gmail.com>
2020-07-10 22:16:40 +0200

    this allows one to write `import resample; resample.jackknife.resample(...)`

--- resample/__init__.py
+++ resample/__init__.py
@@ -3,3 +3,7 @@ resample package.
 """
 
 from .version import version as __version__  # noqa
+from . import jackknife  # noqa
+from . import bootstrap  # noqa
+from . import empirical  # noqa
+from . import permutation  # noqa
