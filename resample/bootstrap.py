abe63496a20c155ca6e9b7424f7d0c20bbd5b178
Daniel Saxton <2658661+dsaxton@users.noreply.github.com>
2020-07-10 14:02:13 -0500

    Don't fix fscale for t distribution (#43)

--- resample/bootstrap.py
+++ resample/bootstrap.py
@@ -214,16 +214,14 @@ def _fit_parametric_family(dist: stats.rv_continuous, sample: np.ndarray) -> Tup
         # has no fit method...
         return np.mean(sample, axis=0), np.cov(sample.T, ddof=1)
 
-    if dist == stats.t:
-        fit_kwd = {"fscale": 1}
-    elif dist in {stats.f, stats.beta}:
-        fit_kwd = {"floc": 0, "fscale": 1}
-    elif dist in (stats.gamma, stats.lognorm, stats.invgauss, stats.pareto):
-        fit_kwd = {"floc": 0}
+    if dist in {stats.f, stats.beta}:
+        fit_kwargs = {"floc": 0, "fscale": 1}
+    elif dist in {stats.gamma, stats.lognorm, stats.invgauss, stats.pareto}:
+        fit_kwargs = {"floc": 0}
     else:
-        fit_kwd = {}
+        fit_kwargs = {}
 
-    return dist.fit(sample, **fit_kwd)
+    return dist.fit(sample, **fit_kwargs)
 
 
 def _resample_parametric(
