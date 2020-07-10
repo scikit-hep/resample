1491a2433a0453aacae33172f58ae38e88c31652
Hans Dembinski <HDembinski@users.noreply.github.com>
2020-07-10 21:03:28 +0200

    Add pytest driven benchmarks (#32)

---
+++ benchmarks/test_jackknife.py
@@ -0,0 +1,26 @@
+import pytest
+import numpy as np
+from resample.jackknife import resample
+from numpy.testing import assert_equal
+
+
+def run_resample(n, copy):
+    x = np.arange(n)
+    r = []
+    for b in resample(x, copy):
+        r.append(np.mean(b))
+    return r
+
+
+@pytest.mark.benchmark(group="jackknife-100")
+@pytest.mark.parametrize("copy", (True, False))
+def test_jackknife_resample_100(benchmark, copy):
+    result = benchmark(run_resample, 100, copy)
+    assert_equal(result, run_resample(100, resample))
+
+
+@pytest.mark.benchmark(group="jackknife-1000")
+@pytest.mark.parametrize("copy", (True, False))
+def test_jackknife_resample_1000(benchmark, copy):
+    result = benchmark(run_resample, 1000, copy)
+    assert_equal(result, run_resample(1000, resample))
