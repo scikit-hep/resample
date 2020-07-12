c2ba18c38d1d28a532eee7b0aca7d1fe47312b0b
Hans Dembinski <hans.dembinski@gmail.com>
2020-07-12 15:18:44 +0200

    inversion test

--- resample/tests/test_empirical.py
+++ resample/tests/test_empirical.py
@@ -1,11 +1,18 @@
 import pytest
 import numpy as np
+from numpy.testing import assert_equal
 
 from resample.empirical import quantile_fn, cdf_fn, influence
 
 
-def test_cdf_increasing():
-    x = np.random.randn(100)
+# high-quality platform-independent reproducible sequence of pseudo-random numbers
+@pytest.fixture
+def rng():
+    return np.random.Generator(np.random.PCG64(1))
+
+
+def test_cdf_increasing(rng):
+    x = rng.normal(size=100)
     cdf = cdf_fn(x)
     result = [cdf(s) for s in np.linspace(x.min(), x.max(), 100)]
     assert np.all(np.diff(result) >= 0)
@@ -31,6 +38,16 @@ def test_quantile_simple_cases():
     assert q(0.5) == 1
     assert q(0.75) == 2
     assert q(1.0) == 3
+
+
+def test_quantile_is_inverse_of_cdf(rng):
+    data = rng.normal(size=100)
+    cdf = cdf_fn(data)
+
+    y = cdf(data)
+
+    quant = quantile_fn(data)
+    assert_equal([quant(yi) for yi in y], data)  # TODO: quant should be vectorized
 
 
 @pytest.mark.parametrize("arg", [-1, 1.5])
