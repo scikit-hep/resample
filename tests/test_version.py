3c39e5b54d3704113037303081c04383baa404e9
Hans Dembinski <hans.dembinski@gmail.com>
2021-05-12 20:58:19 +0200

    fix

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,13 +1,15 @@
-import requests
+from urllib.request import urlopen
 from pkg_resources import parse_version
+import json
 
 
 def test_local_against_pypi_version():
     from resample import __version__
 
+    r = urlopen("https://pypi.org/pypi/resample/json")
+    assert r.code == 200
+    releases = json.loads(r.read())["releases"]
+
     # make sure version is up-to-date
-    pypi_versions = [
-        parse_version(v)
-        for v in requests.get("https://pypi.org/pypi/resample/json").json()["releases"]
-    ]
+    pypi_versions = [parse_version(v) for v in releases]
     assert parse_version(__version__) not in pypi_versions, "pypi version exists"
