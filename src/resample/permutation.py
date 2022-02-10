fix target precision calculation

src/resample/permutation.py
@@ -57,7 +57,8 @@ class TestResult:
         Standard interval (approximately 68 % coverage) for the p-value. This interval
         reflects the statistical uncertainty from doing only a finite number of random
         permutations instead of infinitely many. The interval can be narrowed down by
-        running the test with more permutations.
+        running the test with more permutations. This interval does not have exact
+        coverage for finite samples.
     samples: array
         Values of the test statistic from the permutated samples.
     """
@@ -122,9 +123,10 @@ def usp(
         Two-dimensional array which represents the counts in a histogram. The counts
         can be of floating point type, but must have integral values.
     precision : float, optional
-        Target precision (statistical) for the p-value. The algorithm iteratively
-        increases the number of permutations until the target precision is reached.
-        If precision is zero, the algorithm uses max_size permutations. Default 0.01.
+        Target precision (statistical) for the p-value. The algorithm estimates the
+        number of required permutations to reach the target precision. The accuracy of
+        the actual value may be above or below this value. If precision is zero,
+        max_size permutations are used. Default 0.01.
     max_size : int, optional
         Maximum number of permutations. Default 10000.
     random_state : numpy.random.Generator or int, optional
@@ -168,36 +170,36 @@ def usp(
             ymap[k : k + wij] = iy
             k += wij
 
-    # iteratively generate permutations until target precision is reached
-    ts_total = []
-    n = 0
-    n1 = 0
-    k = max_size if precision == 0 else 10
-    for iter in range(20):
-        k = min(k, max_size - n)
-        if k <= 0:
-            break  # pragma: no cover
-
-        # compute p-value and its uncertainty
-        ts = np.empty(k)
-        for b in range(k):
+    # For Type I error probabilities to hold theoretically, the number of bootstrap
+    # samples drawn may not the depend on the data (comment by Richard Samworth).
+    # However, computing the required number of samples with the worst-case p=0.5
+    # can lead to very pessimistic estimates for the required number of samples,
+    # up to orders of magnitude worse. As a compromise, we burn 10 samples to compute
+    # a crude estimate of the p-value and then use that to draw a fresh sample to
+    # compute the final p-value.
+    if precision > 0:
+        n = 10
+        ts = np.empty(n)
+        for b in range(n):
             rng.shuffle(ymap)
             _fill_w(w, xmap, ymap)
             # m stays the same, since wx and wy remain unchanged
             ts[b] = _usp(f1, f2, w, m)
-        n1 += np.sum(t < ts)
-        n += k
-        ts_total.append(ts)
-        pvalue, interval = _wilson_score_interval(n1, n, 1.0)
+        p = _wilson_center(np.mean(t < ts), len(ts))
+        n = min(int(p * (1 - p) / precision**2), max_size)
+    else:
+        n = max_size
 
-        if precision == 0 or (interval[1] - interval[0]) < 2 * precision:
-            break
+    ts = np.empty(n)
+    for b in range(len(ts)):
+        rng.shuffle(ymap)
+        _fill_w(w, xmap, ymap)
+        # m stays the same, since wx and wy remain unchanged
+        ts[b] = _usp(f1, f2, w, m)
+    npass = np.sum(t < ts)
+    pvalue, interval = _wilson_score_interval(npass, n, 1.0)
 
-        pbar = np.mean(interval)
-        k_projected = int(pbar * (1 - pbar) / precision**2) - n
-        k = np.clip(n // 2, 10 * n, k_projected)
-
-    return TestResult(t, pvalue, interval, np.concatenate(ts_total))
+    return TestResult(t, pvalue, interval, ts)
 
 
 def _fill_w(w, xmap, ymap):
@@ -206,8 +208,7 @@ def _fill_w(w, xmap, ymap):
         w[i, j] += 1
 
 
-# accelerate _fill_w with numba, if it is available,
-# increases speed of usp test 20-30 fold
+# optionally accelerate _fill_w, increases speed of usp test 20-30 fold
 try:
     import numba as nb
 
@@ -260,9 +261,10 @@ def same_population(
         Function with signature f(x) for the test statistic to turn it into a measure of
         deviation. Must be vectorised.
     precision : float, optional
-        Target precision (statistical) for the p-value. The algorithm iteratively
-        increases the number of permutations until the target precision is reached.
-        If precision is zero, the algorithm uses max_size permutations. Default 0.01.
+        Target precision (statistical) for the p-value. The algorithm estimates the
+        number of required permutations to reach the target precision. The accuracy of
+        the actual value may be above or below this value. If precision is zero,
+        max_size permutations are used. Default 0.01.
     max_size : int, optional
         Maximum number of permutations. Default 10000.
     random_state : numpy.random.Generator or int, optional
@@ -307,19 +309,11 @@ def same_population(
 
     joined_sample = np.concatenate(args)
 
-    # iteratively generate of permutations until target precision is reached
-    ts_total = []
-    n = 0
-    n1 = 0
-    k = max_size if precision == 0 else 10
-    for iter in range(20):
-        k = min(k, max_size - n)
-        if k <= 0:
-            break  # pragma: no cover
-
-        # compute p-value and its uncertainty
-        ts = np.empty(k)
-        for b in range(k):
+    # For algorithm below, see comment in usp function.
+    if precision > 0:
+        n = 10
+        ts = np.empty(n)
+        for b in range(n):
             rng.shuffle(joined_sample)
             ts[b] = fn(*(joined_sample[sl] for sl in slices))
         if transform is None:
@@ -328,19 +322,25 @@ def same_population(
         else:
             u = transform(t)
             us = transform(ts)
-        n1 += np.sum(u < us)
-        n += k
-        ts_total.append(ts)
-        pvalue, interval = _wilson_score_interval(n1, n, 1.0)
+        p = _wilson_center(np.mean(u < us), len(us))
+        n = min(int(p * (1 - p) / precision**2), max_size)
+    else:
+        n = max_size
 
-        if precision == 0 or (interval[1] - interval[0]) < 2 * precision:
-            break
+    ts = np.empty(n)
+    for b in range(len(ts)):
+        rng.shuffle(joined_sample)
+        ts[b] = fn(*(joined_sample[sl] for sl in slices))
+    if transform is None:
+        u = t
+        us = ts
+    else:
+        u = transform(t)
+        us = transform(ts)
+    npass = np.sum(u < us)
+    pvalue, interval = _wilson_score_interval(npass, n, 1.0)
 
-        pbar = np.mean(interval)
-        k_projected = int(pbar * (1 - pbar) / precision**2) - n
-        k = np.clip(n // 2, 10 * n, k_projected)
-
-    return TestResult(t, pvalue, interval, np.concatenate(ts_total))
+    return TestResult(t, pvalue, interval, ts)
 
 
 def anova(
@@ -572,3 +572,7 @@ def _wilson_score_interval(n1, n, z):
     a = p + 0.5 * z**2 / n
     b = z * np.sqrt(p * (1 - p) / n + 0.25 * (z / n) ** 2)
     return p, ((a - b) * norm, (a + b) * norm)
+
+
+def _wilson_center(p, n):
+    return (p + 0.5 / n) / (1.0 + 1.0 / n)

tests/test_permutation.py
@@ -193,8 +193,8 @@ def test_usp_2(rng):
 
 def test_usp_3(rng):
     cov = np.empty((2, 2))
-    cov[0, 0] = 2 ** 2
-    cov[1, 1] = 3 ** 2
+    cov[0, 0] = 2**2
+    cov[1, 1] = 3**2
     rho = 0.5
     cov[0, 1] = rho * np.sqrt(cov[0, 0] * cov[1, 1])
     cov[1, 0] = cov[0, 1]
@@ -213,8 +213,8 @@ def test_usp_4():
     r1 = perm.usp(w, precision=0, max_size=1000, random_state=1)
     r2 = perm.usp(np.transpose(w), precision=0, max_size=1000, random_state=1)
     assert_allclose(r1.statistic, r2.statistic)
-    expected = 0.005216488  # according to Richard Samworth, send by email
-    assert_allclose(r1.statistic, expected)
+    expected = 0.004106  # checked against R implementation
+    assert_allclose(r1.statistic, expected, atol=1e-6)
     # according to paper, pvalue is 0.001, but we get 0.0025 in high-statistics runs
     assert_allclose(r1.pvalue, 0.0025, atol=0.001)
     _, interval = perm._wilson_score_interval(0.0025 * 1000, 1000, 1)
@@ -254,7 +254,7 @@ def test_ttest_bad_input():
 
 @pytest.mark.parametrize("test", (perm.ttest, perm.usp))
 @pytest.mark.parametrize("prec", (0, 0.05, 0.005))
-def test_precision_1(test, prec, rng):
+def test_precision(test, prec, rng):
     x = rng.normal(0, 1, size=100)
     y = rng.normal(0, 2, size=100)
     if test is perm.ttest:
@@ -263,9 +263,9 @@ def test_precision_1(test, prec, rng):
         w = np.histogram2d(x, y)[0]
         args = (w,)
 
-    r = test(*args, precision=prec, max_size=10000 if prec > 0 else 123)
+    r = test(*args, precision=prec, max_size=10000 if prec > 0 else 123, random_state=1)
     if prec == 0:
         assert len(r.samples) == 123
     else:
-        assert (r.interval[1] - r.interval[0]) / 2 < prec
-        assert_allclose((r.interval[1] - r.interval[0]) / 2, prec, atol=0.5 * prec)
+        actual_precision = (r.interval[1] - r.interval[0]) / 2
+        assert_allclose(actual_precision, prec, atol=0.25 * prec)